665d207c98d1f23d5560443132c8db8010c6f892
Mike Bond <mikebond84@gmail.com>
2018-08-20 09:09:54 -0400

    no good, but what I have at beginning of class

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const GRID_SIZE = 50
 const TICKS_PER_MOVE = 10
-const PELLET_COUNT = 1
-const COLORS = {
-    wall: '#F6B782',
-    pellets: '#C8553D',
-    GoodSquare: '#F28F3B',
-    background: '#FFD5C2',
-    grid: '#FFFFFF',
-    BulletSquare: '#588B8B',
-    fillStyle: 'green',
-}
-
-const SHOW_GRID = true
+const SHOW_GRID = false
 
 function isSamePos (pos, otherPos) {
     return pos.x === otherPos.x && otherPos.y
@@ -29,7 +18,8 @@ class Game {
     context.gameSize = { x: canvas.width, y: canvas.height }
     this.bodies = []
     this.bodies = this.bodies.concat(new Player(this, gameSize))
-    context.fillRect(50, 50, 50, 50);
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = '#F28F3B'
     context.screenSize = {
           x: this.canvas.width,
           y: this.canvas.height,
@@ -38,7 +28,13 @@ class Game {
         x: canvas.width, 
         y: canvas.height 
     }
-        this.player = new Player(this)
+        posArray = this.pos
+        // this.game = game
+        // this.pos = []
+        // this.x = pos.x
+        // this.y = pos.y
+        // this.pos = ({x: pos.x, y: pos.y})
+        this.player = new Player(this, posArray)
         this.squares = { x: this.width / GRID_SIZE, y: this.height / GRID_SIZE }
         this.bodies = []
         this.gameOver = false
@@ -59,32 +55,32 @@ class Game {
     //     this.tick = tick  
     // }
   draw (screen, gameSize) {
-    screen.clearRect(0, 0, gameSize.x, gameSize.y)
+    this.screen.clearRect(0, 0, gameSize.x, gameSize.y)
     for (let i = 0; i < this.bodies.length; i++) {
         drawRect(screen, this.bodies[i])
       } 
-    this.context.fillRect
-    this.player.draw
-    this.context.fillRect (50, 50, 50, 50)
-    this.screen.clearRect(0, 0, this.size.width, this.size.height)
+    // this.context.fillRect
+    // this.player.draw
+    // this.context.fillRect (50, 50, 50, 50)
+//     this.screen.clearRect(0, 0, this.size.width, this.size.height)
 }
-  drawWall() {
-        if (SHOW_GRID) {
-            this.drawGrid()
-                    }
+//   drawWall() {
+//         if (SHOW_GRID) {
+//             this.drawGrid()
+//                     }
                 
-        for (let pellet of this.pellets) {
-            pellet.draw()
-                    }
-            this.GoodSquare.draw()
-            if (this.gameOver) {
-            this.drawGameOver()
-                    }
-    this.player.draw()
-  }
+//         for (let pellet of this.pellets) {
+//             pellet.draw()
+//                     }
+//             this.Player.draw()
+//             if (this.gameOver) {
+//             this.drawGameOver()
+//                     }
+//     this.Player.draw()
+//   }
   
   update() {
-    this.player.update()
+    this.Player.update()
     while (this.pellets.length < 1) {
         this.placePellet()
       }
@@ -93,76 +89,73 @@ class Game {
   start () {
     this.ticks()
       }
-  checkGameOver () {
-            if (this.isSamePos() || doesIntersectWithArray(this.GoodSquare.BulletSquare(), this.BulletSquare.GoodSquare())) {
-              this.gameOver = true
-            }
-            this.checkGameOver()
-        }
-        drawWall () {
-            this.screen.fillStyle = COLORS.wall
-            this.screen.fillRect(0, 0, this.size.width, this.size.height)
-            this.screen.fillStyle = COLORS.background
-            this.screen.fillRect(
-              GRID_SIZE,
-              GRID_SIZE,
-              (this.squares.x - 2) * GRID_SIZE,
-              (this.squares.y - 2) * GRID_SIZE)
-          }
+//   checkGameOver () {
+//             if (this.isSamePos() || doesIntersectWithArray(this.Player.BulletSquare(), this.BulletSquare.Player())) {
+//               this.gameOver = true
+//             }
+//             this.checkGameOver()
+        // }
+        // drawWall () {
+        //     this.screen.fillStyle = 'blue'
+        //     this.screen.fillRect(0, 0, this.size.width, this.size.height)
+        //     this.screen.fillStyle = 'pink'
+        //     this.screen.fillRect(
+        //       GRID_SIZE,
+        //       GRID_SIZE,
+        //       (this.squares.x - 2) * GRID_SIZE,
+        //       (this.squares.y - 2) * GRID_SIZE)
+        //   }
         
-          drawGrid () {
-            this.screen.strokeStyle = COLORS.grid
-            this.screen.lineWidth = 1
-            this.screen.beginPath()
-            for (var x = 0; x < this.size.width; x += GRID_SIZE) {
-              this.screen.moveTo(x, 0)
-              this.screen.lineTo(x, this.size.height)
-            }
+        //   drawGrid () {
+        //     this.screen.strokeStyle = 'orange'
+        //     this.screen.lineWidth = 1
+        //     this.screen.beginPath()
+        //     for (var x = 0; x < this.size.width; x += GRID_SIZE) {
+        //       this.screen.moveTo(x, 0)
+        //       this.screen.lineTo(x, this.size.height)
+        //     }
         
-            for (var y = 0; y < this.size.height; y += GRID_SIZE) {
-              this.screen.moveTo(0, y)
-              this.screen.lineTo(this.size.width, y)
-            }
+        //     for (var y = 0; y < this.size.height; y += GRID_SIZE) {
+        //       this.screen.moveTo(0, y)
+        //       this.screen.lineTo(this.size.width, y)
+        //     }
         
-            this.screen.stroke()
-          }
+        //     this.screen.stroke()
+        //   }
 
-          drawGameOver () {
-            this.screen.textAlign = 'center'
-            this.screen.font = '48px Helvetica'
-            this.screen.fillStyle = COLORS.wall
-            this.screen.fillText('game over', this.size.width / 2, this.size.height / 2)
-          }
+        //   drawGameOver () {
+        //     this.screen.textAlign = 'center'
+        //     this.screen.font = '48px Helvetica'
+        //     this.screen.fillStyle = COLORS.wall
+        //     this.screen.fillText('game over', this.size.width / 2, this.size.height / 2)
+        //   }
 
-        placePellet () {
-        let foundValidPos = false
-        let pos
-        while (!foundValidPos) {
-            pos = {
-                x: Math.floor(Math.random() * (this.squares.x - 2)) + 1,
-                y: Math.floor(Math.random() * (this.squares.y - 2)) + 1
-    }
-        foundValidPos = !(doesIntersectWithArray(pos, this.Player) ||
-                        doesIntersectWithArray(pos, this.pellets))
-    }
-        this.pellets.push(new Pellet(this, pos))
-    }
+        // placePellet () {
+        // let foundValidPos = false
+        // let pos
+        // while (!foundValidPos) {
+        //     pos = {
+        //         x: Math.floor(Math.random() * (this.squares.x - 2)) + 1,
+        //         y: Math.floor(Math.random() * (this.squares.y - 2)) + 1
+    // }
+    //     foundValidPos = (!doesIntersectWithArray(pos, this.Player) ||
+    //                     doesIntersectWithArray(pos, this.pellets))
+    // }
+    //     this.pellets.push(new Pellet(this, pos))
+    // }
 
 }
 
 //     //lines 138-162
 class Player {
-    constructor(game) {
-        this.center = {
-          x: 50,
-          y: 50
-        }
-        this.playerSize = {
-          x: 20,
-          y: 20
-        }
-        this.keyboarder = new Keyboarder()
+    constructor(game, pos) {
+        // this.keyboarder = new Keyboarder()
         this.game = game
+        this.pos = []
+        this.x = pos.x
+        this.y = pos.y
+        this.pos = ({x: pos.x, y: pos.y})
+        
         console.log = ('hi')
       }
       draw() {
